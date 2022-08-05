22f0fec81d50780b84484bc1237bc7b8ac99cfd2
Vu Anh <anhv.ict91@gmail.com>
2022-08-05 15:06:32 +0700

    update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,44 +1,60 @@
 package com.example.myapplication
 
+import android.content.AsyncQueryHandler
 import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.loader.content.AsyncTaskLoader
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button1.setOnClickListener {
-            Task().execute()
+            ReadContentURL().execute("https://khoapham.vn/")
         }
     }
 
-    inner class Task : AsyncTask<Void, String, String>() {
-        override fun onPreExecute() {
-            super.onPreExecute()
-            textView2.text = "Pre Execute \n"
-        }
-        override fun doInBackground(vararg p0: Void?): String {
-            Log.d("AA", "Hello")
-            for(task in 1..5){
-                publishProgress("Doing $task\n")
+    inner class ReadContentURL: AsyncTask<String, Void, String>(){
+        override fun doInBackground(vararg p0: String?): String {
+            var content: StringBuilder = StringBuilder()
+            var url: URL = URL(p0[0])
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            val inputStream: InputStream = connection.inputStream
+            val reader: InputStreamReader = InputStreamReader(inputStream)
+            val buffer: BufferedReader = BufferedReader(reader)
+            var line: String = ""
+            try {
+                do {
+                    line = buffer.readLine()
+                    if(line != null){
+                        content.append(line)
+                    }
+                } while (line != null)
+                buffer.close()
+            } catch (e: Exception) {
+                Log.d("AAA", e.toString())
             }
-            return "Done"
+            return content.toString()
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            textView2.append(result)
-        }
-
-        override fun onProgressUpdate(vararg values: String?) {
-            super.onProgressUpdate(*values)
-            textView2.append(values[0])
+            Toast.makeText(this@MainActivity, result.toString(), Toast.LENGTH_LONG).show()
         }
 
     }
+
 }
