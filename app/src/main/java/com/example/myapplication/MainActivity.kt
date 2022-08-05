280d50624bff3079ccb7111400c50b90441ba5c0
Vu Anh <anhv.ict91@gmail.com>
2022-08-05 15:27:11 +0700

    update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,45 +1,29 @@
 package com.example.myapplication
 
-import android.content.AsyncQueryHandler
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.loader.content.AsyncTaskLoader
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.lang.Exception
-import java.lang.StringBuilder
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        LoadImage().execute("https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg")
+        val url: String = "https://i.imgur.com/DvpvklR.png"
+        val urlFail: String = "https://i.imgur.com/DvpvklR111.png1111"
+        // case 1: Normal
+//        Picasso.get().load(url).into(imageView);
+        // case 2: With loading
+//        Picasso.get()
+//            .load(url)
+//            .placeholder(R.drawable.image_loading)
+//            .error(R.drawable.image_fail)
+//            .into(imageView);
+        // case 3: Loading fail
+        Picasso.get()
+            .load(urlFail)
+            .placeholder(R.drawable.image_loading)
+            .error(R.drawable.image_fail)
+            .into(imageView);
     }
-
-    inner class LoadImage: AsyncTask<String, Void, Bitmap>(){
-        override fun doInBackground(vararg p0: String?): Bitmap {
-            val url = URL(p0[0])
-            val inputStream = url.openConnection().getInputStream()
-            val bitmap:Bitmap = BitmapFactory.decodeStream(inputStream)
-            return bitmap
-        }
-
-        override fun onPostExecute(result: Bitmap?) {
-            super.onPostExecute(result)
-            imageView.setImageBitmap(result)
-        }
-
-    }
-
 }
