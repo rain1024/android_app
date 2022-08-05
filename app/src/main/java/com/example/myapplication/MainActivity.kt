79192487268a6ea618759bd95b6cff869adc502e
Vu Anh <anhv.ict91@gmail.com>
2022-08-05 14:45:13 +0700

    update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,14 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button1.setOnClickListener {
-            var intent: Intent = Intent(applicationContext, SecondActivity::class.java)
-//            intent.putExtra("data", "my name")
-//            intent.putExtra("number", 1)
-            val arrayNumber: IntArray = intArrayOf(10, 20, 30, 40)
-            val dish: Dish = Dish("bread", 0)
-//            intent.putExtra("array", arrayNumber)
-            intent.putExtra("data", dish)
-            startActivity(intent)
+            Task().execute()
         }
+    }
+
+    inner class Task : AsyncTask<Void, String, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            textView2.text = "Pre Execute \n"
+        }
+        override fun doInBackground(vararg p0: Void?): String {
+            Log.d("AA", "Hello")
+            for(task in 1..5){
+                publishProgress("Doing $task\n")
+            }
+            return "Done"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            textView2.append(result)
+        }
+
+        override fun onProgressUpdate(vararg values: String?) {
+            super.onProgressUpdate(*values)
+            textView2.append(values[0])
+        }
+
     }
 }
